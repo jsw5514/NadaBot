577a7ec60f03244419eab6f5e15f15222c15e7f4
jsw5514 <jsw55514@naver.com>
2025-04-07 00:03:33 +0900

    토큰 보안처리+ !ping 명령 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("net.dv8tion:JDA:5.0.0-beta.13")
+    implementation("org.slf4j:slf4j-api:1.7.36") // SLF4J API
+    implementation("ch.qos.logback:logback-classic:1.2.10") // Logback 바인딩
 }
-
 tasks.test {
     useJUnitPlatform()
 }
--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,5 +1,11 @@
 package org.example;
 
+//토큰 별도 저장용
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -7,14 +13,35 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bot extends ListenerAdapter {
     public static void main(String[] args) throws Exception {
-        JDABuilder.createDefault("YOUR_DISCORD_BOT_TOKEN")
-                .enableIntents(
-                        GatewayIntent.MESSAGE_CONTENT,
-                        GatewayIntent.GUILD_MESSAGES,
-                        GatewayIntent.GUILD_VOICE_STATES
-                )
-                .addEventListeners(new Bot())
-                .build();
+//        String currentDirectory = System.getProperty("user.dir");
+//        System.out.println("현재 작업 디렉터리: " + currentDirectory);
+//        System.out.println("Hello World!");
+
+        String envFilePath = "src/main/resources/token.env";
+
+        // Properties 객체를 이용해 .env 파일 읽기
+        Properties envProps = new Properties();
+        try {
+            envProps.load(Files.newBufferedReader(Paths.get(envFilePath)));
+            // 프라이빗 토큰 읽기
+            String privateToken = envProps.getProperty("PRIVATE_TOKEN");
+            //System.out.println("프라이빗 토큰: " + privateToken); //debug output
+
+            JDABuilder.createDefault(privateToken)
+                    .enableIntents(
+                            GatewayIntent.MESSAGE_CONTENT,
+                            GatewayIntent.GUILD_MESSAGES,
+                            GatewayIntent.GUILD_VOICE_STATES
+                    )
+                    .addEventListeners(new Bot())
+                    .build();
+
+        } catch (IOException e) {
+            System.err.println(".env 파일을 읽는 도중 에러가 발생했습니다.");
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
